b7f2a29792b5e86c8100da94af02ed25d30a919f
kamisamatenshi <tsecm234@gmail.com>
2019-09-28 04:01:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -232,7 +232,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				} else if strings.Contains(message.Text, "現在幾點") {
 					tellTime(replyToken, true)
 				}else if strings.Contains(message.Text, "try") {
-					linebot.findAndAddContactsByMid("Ub01e3901e840a9dd76a1bf6904a93cee")
+					bot.findAndAddContactsByMid("Ub01e3901e840a9dd76a1bf6904a93cee").Do()
 				} else if "說吧" == message.Text {
 					silentMap[sourceId] = false
 					bot.ReplyMessage(replyToken, linebot.NewTextMessage("麥克風測試，1、2、3... OK")).Do()
