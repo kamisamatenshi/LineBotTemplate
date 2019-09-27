def4474699d13810cd1467397f6af42e0529366e
kamisamatenshi <tsecm234@gmail.com>
2019-09-28 04:48:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -236,11 +236,11 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					silentMap[sourceId] = false
 					bot.ReplyMessage(replyToken, linebot.NewTextMessage("麥克風測試，1、2、3... OK")).Do()
 				} else if "try" == message.Text {
-					profile:= bot.GetProfile(source.UserID).Do()
-					bot.PushMessage(bailei, linebot.NewTextMessage(profile.DisplayName + ": "+message.Text)).Do()
-					
-								
-					 // bot.PushMessage(bailei, linebot.NewTextMessage(user_tenshi.Displayname)).Do()								
+					profile,err:= bot.GetProfile(source.UserID).Do()
+					if err != nil {
+						log.Print(err)
+					}bot.PushMessage(bailei, linebot.NewTextMessage(profile.DisplayName + ": "+message.Text)).Do()
+					// bot.PushMessage(bailei, linebot.NewTextMessage(user_tenshi.Displayname)).Do()								
 				} else if "洗版開始密碼010220" == message.Text {
 					washMap[sourceId] = true
 					bot.ReplyMessage(replyToken, linebot.NewTextMessage("準備完畢")).Do()
