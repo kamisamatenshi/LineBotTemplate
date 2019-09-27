074d8a8a24272aa9d51166e4f710ef72d9f2396d
kamisamatenshi <tsecm234@gmail.com>
2019-09-28 05:03:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -455,19 +455,6 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				} else if strings.Contains(message.Text, "Q") {
 					bot.ReplyMessage(replyToken, linebot.NewTextMessage(answers_ReplyQQ[rand.Intn(len(answers_ReplyQQ))])).Do()
 				} else if AwashMap[sourceId] == true {
-					if message.Text == message.Text{
-						bot.ReplyMessage(replyToken, linebot.NewTextMessage(message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+
-												    message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text+message.Text)).Do()
 					profile,err:= bot.GetProfile(source.UserID).Do()
 					if err != nil {
 						log.Print(err)
@@ -476,7 +463,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					if err2 != nil {
 							log.Print(err2)
 					}
-					bot.PushMessage(bailei, linebot.NewTextMessage(message.Text)).Do()
+					bot.PushMessage(bailei, linebot.NewTextMessage(message.Text+profile.Displayname+profile2.Displayname)).Do()
 					}
 				}else if washMap[sourceId] == true {
 					if highCMap[sourceId] == false{
